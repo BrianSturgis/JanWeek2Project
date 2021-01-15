d6e92bb772c2cb24962d7f4bd88eb1575ab1645d
Brian Sturgis <sturujisu@gmail.com>
2021-01-15 15:17:11 -0800

    finsish writing all 8 inputs into js file

--- js/scripts.js
+++ js/scripts.js
@@ -21,12 +21,20 @@ $(document).ready(function() {
     console.log(question1);
     const userName = $("input#nameOfPerson").val();
     console.log(userName);
-    const question3= $("").val();
-    const question4 = $("").val();
-    const question5 = $("").val();
-    // const question6 = $("").val();
-    // const question7 = $("").val();
-    // const question8 = $("").val();
+    const question2= $("#question2").val();
+    console.log(question2);
+    const question3 = $("#question3").val();
+    console.log(question3);
+    const question4 = $("#question4").val();
+    console.log(question4);
+    const question5 = $("#question5").val();
+    console.log(question5);
+    const question6 = $("#question6").val();
+    console.log(question6);
+    const question7 = $("#question7").val();
+    console.log(question7);
+    const question7 = $("#question8").val();
+    console.log(question7);
 
     // $("").text(question1);
     // $("").text(question2);
