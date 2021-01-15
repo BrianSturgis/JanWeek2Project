34f18202bfd66276c88b5f7b6acdc8d12f28b703
Brian Sturgis <sturujisu@gmail.com>
2021-01-15 11:58:25 -0800

    event listeners to JS file

--- js/scripts.js
+++ js/scripts.js
@@ -6,19 +6,25 @@
 
 
 // ui logic
-$(document).ready(function(){
-  $("").submit(function(event){
+(document).ready(function() {
+  $("#formOne").submit(function(event) {
     event.preventDefault();
+    const question1 = $("").val();
+    const question2 = $("").val();
+    const question3= $("").val();
+    const question4 = $("").val();
+    const question5 = $("").val();
+    const question6 = $("").val();
+    const question7 = $("").val();
+    const question8 = $("").val();
 
+    $("").text(question1);
+    $(".").text(question2);
+    $(".").text(question3);
+    $(".").text(question4);
+    $(".").text(question5);
+    $(".").text(question6);
 
-
-
-
-
-
-
-
-
-
+    $("#story").show();
   });
-});?
+});
