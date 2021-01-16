298d3e19042795f535c59bedbfa9be7f80e2abb6
Brian Sturgis <sturujisu@gmail.com>
2021-01-15 17:23:24 -0800

    edit the js file one last time before testing

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
 // business logic
 function totalValue (num1,num2,num3,num4,num5,num6,num7,num8) {
-  return parseInt(num1) + parseInt(num2) + parseInt(num3) + parseInt(num4) + parseInt(num5) + parseInt(num6) + parseInt(num7) + parseInt(num8)
+  return parseInt(num1) + parseInt(num2) + 
+  parseInt(num3) + parseInt(num4) + 
+  parseInt(num5) + parseInt(num6) + 
+  parseInt(num7) + parseInt(num8)
 }
 
 
@@ -10,9 +13,9 @@ function totalValue (num1,num2,num3,num4,num5,num6,num7,num8) {
 $(document).ready(function() {
   $("form#questions").submit(function(event) {
     event.preventDefault();
-    const question1 = $("#question1").val();
     const userName = $("input#nameOfPerson").val();
-    const question2= $("#question2").val();
+    const question1 = $("#question1").val();
+    const question2 = $("#question2").val();
     const question3 = $("#question3").val();
     const question4 = $("#question4").val();
     const question5 = $("#question5").val();
@@ -31,21 +34,22 @@ $(document).ready(function() {
       // $("#").show();
       // $("#").hide();
       // $("#").hide();
-      console.log("15");
+      console.log("25");
     } else if (total >= 23) {
       // $("#").show();
       // $("#").hide();
       // $("#").hide();
-      console.log("8");
+      console.log("23");
     } else if (total >= 20) {
       // $("#").show();
       // $("#").hide();
       // $("#").hide();
+      console.log("20");
     } else if (total >=17) {
       // $("#").show();
       // $("#").hide();
       // $("#").hide();
-      console.log("15");
+      console.log("17");
     } else if (total >=15) {
       // $("#").show();
       // $("#").hide();
@@ -55,12 +59,12 @@ $(document).ready(function() {
       // $("#").show();
       // $("#").hide();
       // $("#").hide();
-      console.log("15");
+      console.log("12");
     } else if (total >=9) {
       // $("#").show();
       // $("#").hide();
       // $("#").hide();
-      console.log("15");
+      console.log("9");
     } else (alert("PLEASE ANSWER THE QUESTION TO COMPLETE THE TEST"));
     // console.log(total);
 
